68b877e1d744c157d1c6c19744522ff128f23f0d
Boaz Wu <boazwu@outlook.com>
2022-02-10 21:43:47 -0800

    ui testing

--- src/main/java/io/worduel/worduelapi/HomePage.java
+++ src/main/java/io/worduel/worduelapi/HomePage.java
@@ -9,26 +9,19 @@ import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.Route;
 
-@Route("") 
-public class HomePage extends VerticalLayout { 
+@Route("")
+public class HomePage extends VerticalLayout {
 
-  public HomePage() {
-    VerticalLayout todosList = new VerticalLayout(); 
-    TextField taskField = new TextField(); 
-    Button addButton = new Button("Add"); 
-    addButton.addClickListener(click -> { 
-      Checkbox checkbox = new Checkbox(taskField.getValue());
-      todosList.add(checkbox);
-    });
-    addButton.addClickShortcut(Key.ENTER); 
+	public HomePage() {
+		VerticalLayout todosList = new VerticalLayout();
+		TextField taskField = new TextField();
+		Button addButton = new Button("Add");
+		addButton.addClickListener(click -> {
+			Checkbox checkbox = new Checkbox(taskField.getValue());
+			todosList.add(checkbox);
+		});
+		addButton.addClickShortcut(Key.ENTER);
 
-    add( 
-      new H1("Worduel"),
-      todosList,
-      new HorizontalLayout(
-        taskField,
-        addButton
-      )
-    );
-  }
+		add(new H1("Worduel"), todosList, new HorizontalLayout(taskField, addButton));
+	}
 }
