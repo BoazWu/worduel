905dda23b50fe255c89fab83253e6aa7950337a9
R <dsammsh@gmail.com>
2022-02-12 16:49:37 -0800

    Created basic lobby structure

---
+++ src/main/java/io/worduel/worduelapi/Model/RoomManager.java
@@ -0,0 +1,15 @@
+package io.worduel.worduelapi.Model;
+
+import java.util.HashMap;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class RoomManager {
+	private HashMap<String, Room> roomMap = new HashMap<String, Room>();
+	
+	public void addRoom() {
+		
+	}
+	
+}
